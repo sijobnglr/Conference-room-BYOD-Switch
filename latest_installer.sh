2fcd5225e2161d856b61c24bdd4788e01fa6c2dd
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2023-07-10 19:40:14 +0530

    Update latest_installer.sh

--- latest_installer.sh
+++ latest_installer.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-wget https://raw.githubusercontent.com/sijobnglr/Conference-room-BYOD-Switch/main/Updated_Installer.sh && wget https://raw.githubusercontent.com/sijobnglr/Conference-room-BYOD-Switch/main/Install_nodes.sh
+wget https://raw.githubusercontent.com/sijobnglr/Conference-room-BYOD-Switch/main/Updated_Installer.sh && wget https://raw.githubusercontent.com/sijobnglr/Conference-room-BYOD-Switch/main/Install_nodes.sh && sudo chmod +x latest_installer.sh && sudo bash latest_installer.sh && sudo chmod +x Updated_Installer.sh  
 
