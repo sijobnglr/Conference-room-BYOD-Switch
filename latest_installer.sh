6799ad15044d785db02994c6d26ce7f8e9f189c5
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2024-12-12 09:50:03 +0530

    Update latest_installer.sh

--- latest_installer.sh
+++ latest_installer.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-wget https://github.com/sijobnglr/Conference-room-BYOD-Switch/blob/main/Updated_Installer.sh && sudo chmod +x Updated_Installer.sh && sudo bash Updated_Installer.sh
+wget https://github.com/sijobnglr/Conference-room-BYOD-Switch/blob/main/mode_sw_installer_latest.sh && sudo chmod +x Updated_Installer.sh && sudo bash Updated_Installer.sh
 
